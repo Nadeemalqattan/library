dea5c99693b5e827015c7fa6f896de63357e1c5a
hesham <hesham@heshams-iMac.local>
2021-02-04 11:57:24 +0300

    added members form

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -1,63 +1,78 @@
-// import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { createMember } from "../store/actions";
-// import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { ListWrapper } from "../styles";
+import { useDispatch } from "react-redux";
+import { createMember } from "../store/actions";
+import { useHistory } from "react-router-dom";
 
-// const MemberForm = () => {
-//   const [member, setMember] = useState({
-//     firstName: "",
-//     lastName: "",
-//     membership: "",
-//   });
+const MemberForm = () => {
+  const [member, setMember] = useState({
+    firstName: "",
+    lastName: "",
+    currentlyBorrowedBooks: "",
+    membership: "",
+  });
 
-//   const history = useHistory();
+  const history = useHistory();
 
-//   const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(createMember(member));
+    history.push("/members");
+  };
+  const handleChange = (event) => {
+    setMember({ ...member, [event.target.name]: event.target.value });
+  };
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     dispatch(createMember(member));
-//     history.push("/members");
-//   };
-//   const handleChange = (event) => {
-//     setMember({ ...member, [event.target.name]: event.target.value });
-//   };
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div className="form-group row">
-//         <div className="col-6">
-//           <label>First Name</label>
-//           <input
-//             value={member.firstName}
-//             name="firstName"
-//             type="text"
-//             className="form-control"
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <div className="col-6">
-//           <label>Last Name</label>
-//           <input
-//             value={member.lastName}
-//             name="lastName"
-//             type="text"
-//             className="form-control"
-//             onChange={handleChange}
-//           />
-//         </div>
-//       </div>
-//       <div className="form-group">
-//         <label>Membership</label>
-//         <input
-//           value={member.membership}
-//           name="membership"
-//           type="text"
-//           className="form-control"
-//           onChange={handleChange}
-//         />
-//       </div>
-//       <button className="btn float-right">Create</button>
-//     </form>
-//   );
-// };
-// export default MemberForm;
+  return (
+    <ListWrapper>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group row">
+          <div className="col-6">
+            <label>First Name</label>
+            <input
+              value={member.firstName}
+              name="firstName"
+              type="text"
+              className="form-control"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="col-6">
+            <label>Last Name</label>
+            <input
+              value={member.lastName}
+              name="lastName"
+              type="text"
+              className="form-control"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Borrowed Books</label>
+          <input
+            value={member.currentlyBorrowedBooks}
+            name="currentlyBorrowedBooks"
+            type="number"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Membership</label>
+          <input
+            value={member.membership}
+            name="membership"
+            type="text"
+            className="form-control"
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn float-right">Create</button>
+      </form>
+    </ListWrapper>
+  );
+};
+
+export default MemberForm;
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,16 @@
 const CREATE_BOOK = "CREATE_BOOK";
+const CREATE_MEMBER = "CREATE_MEMBER";
 
 export const createBook = (newBook) => {
   return {
     type: CREATE_BOOK,
     payload: { newBook },
+  };
+};
+
+export const createMember = (newMember) => {
+  return {
+    type: CREATE_MEMBER,
+    payload: { newMember },
   };
 };
